cf6923e0cf4f240cd97c8ac9d8e754397a1e744d
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-17 15:38:53 -0800

    video fromusers camera displays on page

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+var video = document.querySelector("#videoElement");
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+
+function videoError(e) {
+    // do something
+}
